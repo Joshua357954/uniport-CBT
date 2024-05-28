88cda420f5ee5c0a68e4bb35cc679977c615ac21
joshua boyi <boyijoshua72@gmail.com>
2024-05-28 11:22:50 +0100

    new

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,19 @@
+import Head from "next/head";
 
+export const metadata = {
+  title: "CBT-BOX",
+  description: "CBT BOX - C.Sc Help Group (Built by Joshua Boyi)",
+  icon:'/favicon.svg'
+};
 
-export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+export default function Layout({ children }) {
+  return (
+    <html>
+      <Head>   
+        <title>CBT BOX - C.Sc Help Group (Built by Joshua Boyi)</title>
+      </Head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
